0c7136c6927583a75af621ead4629da67f5b46c2
Smit Shah <who828@gmail.com>
2013-06-26 15:28:37 +0530

    Implemented a memoised edit_distance method

--- src/string_matching/core.clj
+++ src/string_matching/core.clj
@@ -1,6 +1,23 @@
-(ns string-matching.core)
+(ns ^{:doc "Approximate String Matching using Levenshtein Distance algorithm."
+      :author "Smit Shah <who828@gmail.com>"}
+  string-matching.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- take-first [s]
+  (take (- (count s) 1) s))
+
+(defn- min-edit-distance [t p cost]
+  (min (+ (edit-distance (take-first t) p) 1)
+       (+ (edit-distance t (take-first p)) 1)
+       (+ (edit-distance (take-first t) (take-first p)) cost)))
+
+(defn- edit-dist [t p]
+  (cond
+   (empty? t) (count p)
+   (empty? p) (count t)
+   (= (last t) (last p)) (min-edit-distance t p 0)
+   :else (min-edit-distance t p 1)))
+
+(def edit-distance
+  "Get minimum distance between two strings"
+  (memoize edit-dist))
+
