3c7d7807dd448b618fa30b02725a7bbf0295661d
Smit Shah <who828@gmail.com>
2013-06-26 19:32:35 +0530

    Updated project information

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject string_matching "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject string_matching "0.0.1"
+  :description "Approximate String Matching using Levenshtein Distance algorithm"
+  :url "http://github.com/who828"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
