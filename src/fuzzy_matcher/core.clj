33c1e0b0e6c4c9ba376b9df14a04ed22a3b526c1
Smit Shah <who828@gmail.com>
2013-06-28 00:12:05 +0530

    Refactored the search function

--- src/fuzzy_matcher/core.clj
+++ src/fuzzy_matcher/core.clj
@@ -6,16 +6,16 @@
 
 (defn- edit-distance* [t p]
   (cond
-    (empty? t) (count p)
-    (empty? p) (count t)
-    (= (last t) (last p)) (min-edit-distance t p 0)
-    :else (min-edit-distance t p 1)))
+   (empty? t) (count p)
+   (empty? p) (count t)
+   (= (last t) (last p)) (min-edit-distance t p 0)
+   :else (min-edit-distance t p 1)))
 
 
 (defn- min-edit-distance [t p cost]
-  (min (+ (edit-distance (butlast t) p) 1)
-       (+ (edit-distance t (butlast p)) 1)
-       (+ (edit-distance (butlast t) (butlast p)) cost)))
+  (min (+ (edit-distance* (butlast t) p) 1)
+       (+ (edit-distance* t (butlast p)) 1)
+       (+ (edit-distance* (butlast t) (butlast p)) cost)))
 
 
 (def edit-distance
@@ -23,9 +23,7 @@
   (memoize edit-distance*))
 
 
-(defn search [word lst & {:keys [n rank] :or {n 15 rank 2}}]
+(defn search [word lst & {:keys [rank] :or {rank 3}}]
   "Get a list of words based on the minimum distance"
-  (let [ranked-words (apply hash-map
-                            (mapcat (fn [x] [x (edit-distance word x)]) lst))]
-    (take n (keys (sort-by val < (filter #(<= (second %) rank)
-                                         ranked-words))))))
+  (sort-by #(edit-distance word %) < (filter #(<= (edit-distance word %) rank)
+                                             lst)))
